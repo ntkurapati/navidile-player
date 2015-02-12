ba976099616e5f16be3219d5093e9a21bde4a524
ntkurapati <ntkurapati@users.noreply.github.com>
2015-02-11 21:30:09 -0500

    new alerts.php

---
+++ alerts.php
@@ -0,0 +1,27 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+$username = "navidileappphp";
+$password = ini_get("mysqli.default_pw");
+$hostname = "localhost";
+$mysqli = new mysqli($hostname, $username, $password, "pittmedstech_nav");
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+$pw=$_GET['id'];
+
+$statement = $mysqli->prepare("SELECT `email_addr`, `subscriptions`, `class_years`,  FROM recordings WHERE `password` = ?");
+$statement->bind_param('s', $pw);
+$statement->execute();
+$statement->bind_result($email_addr, $subscriptions, $class_years);
+while($statement->fetch()){
+{
+    echo($email_addr)
+    echo($subscriptions)
+    echo($class_years)
+}
+$statement->free_result();
+
+?>
