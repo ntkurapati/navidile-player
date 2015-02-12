b0b7fbd6fb13b0b3c3b6c9c80603538d41b1bf91
ntkurapati <ntkurapati@users.noreply.github.com>
2015-02-12 07:38:31 -0500

    add pages for subscribe

---
+++ subscribe.php
@@ -0,0 +1,11 @@
+<html>
+ <body>
+
+<form action="confirm.php" method="post">
+ E-mail: <input type="text" name="email"><br>
+  alerts: <input type="text" name="email"><br>
+ <input type="submit">
+ </form>
+
+</body>
+ </html>
